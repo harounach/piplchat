87dcf3bf3ce80bb0068ffdd8afc2948cd737496f
Haroun Ach <harounacho21@gmail.com>
2021-10-12 13:26:30 +0100

    Adjust message padding and margin

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,24 +2,28 @@ import React from "react";
 
 const Message = ({ recepient, messageEntry }) => {
   const senderView = (
-    <div className="flex w-full">
+    <div className="flex w-full items-start">
       {/* avatar */}
       <div className="w-14 h-14 bg-blue-400 rounded-full flex justify-center items-center flex-shrink-0 overflow-hidden mr-2">
         <img src={messageEntry.photoURL} alt={messageEntry.name + " Photo"} />
       </div>
       {/* Message */}
-      <p className="bg-blue-200 p-2 rounded-lg">{messageEntry.message}</p>
+      <p className="bg-blue-200 px-4 py-2 mt-2 rounded-lg">
+        {messageEntry.message}
+      </p>
     </div>
   );
 
   const recepientVeiw = (
-    <div className="flex w-full flex-row-reverse">
+    <div className="flex w-full flex-row-reverse items-start ">
       {/* avatar */}
       <div className="w-14 h-14 bg-blue-400 rounded-full flex justify-center items-center flex-shrink-0 overflow-hidden ml-2">
         <img src={messageEntry.photoURL} alt={messageEntry.name + " Photo"} />
       </div>
       {/* Message */}
-      <p className="bg-gray-200 p-2 rounded-lg">{messageEntry.message}</p>
+      <p className="bg-gray-200 px-4 py-2 mt-2 rounded-lg">
+        {messageEntry.message}
+      </p>
     </div>
   );
 
